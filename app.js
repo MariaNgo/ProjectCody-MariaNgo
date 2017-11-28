2740211ad917a84a0ce280e4d439d7043e1bf277
Maria Ngo <mariango161@gmail.com>
2017-11-28 13:57:36 +1100

    addMessage function to post users' input

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-function myFunction() {
-  document.getElementById("form_1").submit();
+function addMessage() {
+  var userInput = document.getElementById("messages");
+
+  console.log(userInput.value);
+
+  var newPara = document.createElement("P");
+  var postToNewsFeed = document.createTextNode(userInput.value);
+
+  newPara.appendChild(postToNewsFeed);
+
+  var newsfeed = document.getElementById("post");
+
+  newsfeed.appendChild(newPara);
+
+  // Form clear
+  document.getElementById("form_messages").reset();
+
+
+/*  newText.appendChild(postToNewsFeed);
+  document.body.appendChild(newText);
+*/
+
 }
