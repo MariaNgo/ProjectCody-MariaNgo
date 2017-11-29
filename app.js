a1d4687d896c654b7a6c8949f642a2e82f6d7a1c
Maria Ngo <mariango161@gmail.com>
2017-11-29 13:59:52 +1100

    Post messages to firebase

--- app.js
+++ app.js
@@ -15,9 +15,33 @@ function addMessage() {
   // Form clear
   document.getElementById("form_messages").reset();
 
-
 /*  newText.appendChild(postToNewsFeed);
   document.body.appendChild(newText);
 */
+}
 
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCvRk801MNuTBSOxeXrWB-B4GBjN9_6Llo",
+  authDomain: "projectcodymariango.firebaseapp.com",
+  databaseURL: "https://projectcodymariango.firebaseio.com",
+  projectId: "projectcodymariango",
+  storageBucket: "",
+  messagingSenderId: "975071454761"
+};
+firebase.initializeApp(config);
+
+/*
+// Create a new reference to a location (folder) named messages
+let messagesRef = firebase.database().ref("stored_messages");
+
+// Create a new messgae in my firebase
+messagesRef.push().set(saveData)
+*/
+
+function writeUserData(postToNewsFeed) {
+  firebase.database().ref("stored_messages").set({
+    data: postToNewsFeed
+  });
 }
